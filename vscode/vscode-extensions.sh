95fc1600ed31b4aac5b437adf90f3604a150413f
Nathaniel Hall <nathaniel.hall@gmail.com>
2021-02-07 11:05:45 -0500

    extensions / font

---
+++ vscode/vscode-extensions.sh
@@ -0,0 +1,28 @@
+# https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+code --install-extension dbaeumer.vscode-eslint
+
+# https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+code --install-extension esbenp.prettier-vscode
+
+# https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer-2
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+
+# https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens
+code --install-extension eamodio.gitlens
+
+# https://marketplace.visualstudio.com/items?itemName=vscode-icons-team.vscode-icons
+code --install-extension vscode-icons-team.vscode-icons
+
+# https://marketplace.visualstudio.com/items?itemName=wayou.vscode-todo-highlight
+code --install-extension wayou.vscode-todo-highlight
+
+# https://marketplace.visualstudio.com/items?itemName=wesbos.theme-cobalt2
+code --install-extension wesbos.theme-cobalt2
+
+# https://marketplace.visualstudio.com/items?itemName=vscodevim.vim
+code --install-extension vscodevim.vim
+
+
+echo 
+echo "listing all extensions installed..."
+code --list-extensions
