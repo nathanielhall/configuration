d1a33716c75e44e3f528dc3d07297a82f9f4f9cc
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2021-09-26 07:17:33 -0400

    lua init file

---
+++ .config/nvim/lua/nathanielhall/init.lua
@@ -0,0 +1,10 @@
+require("nathanielhall._telescope")
+
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
