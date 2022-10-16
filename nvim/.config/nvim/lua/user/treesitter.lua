2e2ee07f49b1c43665be7a5cfe5af2cb035e153a
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2022-10-16 19:28:40 -0400

    formatting for go and rust

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -4,11 +4,9 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "typescript", "javascript", "tsx", "jsdoc", "go" }, -- put the language you want in this array
-  -- ensure_installed = "all", -- one of "all" or a list of languages
+  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "typescript", "javascript", "tsx", "jsdoc", "go", "rust" }, -- put the language you want in this array
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
   highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
